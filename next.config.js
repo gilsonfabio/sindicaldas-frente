a7f49c48f4b6d1ef4a8fef4ff0b1fc989655acf1
Gilson <gilsonfabio@gmail.com>
2022-08-03 22:23:33 -0300

    Alteração do next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ module.exports = {
         {
           source: '/:path',
           headers: [
-            {key: 'Access-Control-Allow-Credentials', value: true},
             {key: 'Access-Control-Allow-Origin', value: '*'},
             {key: 'Access-Control-Allow-Methods', value: 'GET, PUT, POST, DELETE'},
             {key: 'Access-Control-Allow-Headers', value: 'X-PINGOTHER, Content-Type, Authorization'},
