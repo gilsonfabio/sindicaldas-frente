f1b763a01b370676a018b9bfa0e2e51c607b0840
Gilson Fábio <gilsonfabio@gmail.com>
2022-12-14 15:38:42 -0300

    Alteração da chamada da api https

--- pages/api/api.js
+++ pages/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: 'https://backend-sindicaldas.herokuapp.com' });
+const api = axios.create({baseURL: 'https://innvento2120.c37.integrator.host'});
 //const api = axios.create({baseURL: 'http://localhost:3333' });
 
 export default api;
